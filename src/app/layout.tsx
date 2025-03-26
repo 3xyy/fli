9042031a2a6daba17ed993b6801b7200a71f4031
3xy <77691604+3xyy@users.noreply.github.com>
2025-03-26 09:56:52 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ClientBody from "./ClientBody";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
       <ClientBody>
         {children}
       </ClientBody>
+      <Analytics />
     </html>
   );
 }
